c4e02831b80d753c5023f11b7fe1ed7a2a830524
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-02 12:08:54 +0545

    implemented .env/and fetched city name along with temp from the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/pages/weather_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -15,5 +17,4 @@ class MyApp extends StatelessWidget {
       home: WeatherPage(),
     );
   }
-
 }
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -9,8 +11,53 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  late final WeatherService _weatherService;
+  Weather? _weather;
+
+  // Fetch weather
+  Future<void> _fetchWeather() async {
+    try {
+      String cityName = await _weatherService.getCurrentCity();
+      Weather weather = await _weatherService.fetchWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print('Error fetching weather: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    final apiKey = dotenv.env['WEATHER_API_KEY'];
+    if (apiKey == null || apiKey.isEmpty) {
+      throw Exception('WEATHER_API_KEY not found in .env');
+    }
+
+    _weatherService = WeatherService(apiKey);
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // city name
+            Text(_weather?.cityName ?? 'Loading City...'),
+
+            // temperature
+            if (_weather != null)
+              Text('${_weather!.temperature.round()}°C')
+            else
+              Text('Loading Temperature...'),
+          ],
+        ),
+      ),
+    );
   }
 }
