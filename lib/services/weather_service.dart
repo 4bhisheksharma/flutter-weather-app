575206b83c662ccefa8f9c2067b6dbf7a040f87c
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-02 09:51:33 +0545

    created services to fetch weather and to get current city

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,47 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WeatherService {
+  static const BaseURL = "http://api.openweathermap.org/data/2.5/weather";
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> fetchWeather(String city) async {
+    final url = Uri.parse('$BaseURL?q=$city&appid=$apiKey&units=metric');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return Weather.fromJson(data);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    // to get location permission from the user
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    //fetch current location
+    Position position = await Geolocator.getCurrentPosition();
+    desiredAccuracy:
+    LocationAccuracy.high;
+
+    //convert the location into a list of placemarks
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude, position.longitude);
+
+    //extract the city name from the placemarks
+    String? city = placemarks.first.locality;
+
+    return city ?? 'Unknown City';
+
+  }
+}
